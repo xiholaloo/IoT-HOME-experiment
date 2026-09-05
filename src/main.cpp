637d4837f9bd9ad8d476e0733337b7ebf157a0b2
jiva <xiholaloo@gmail.com>
2026-09-05 16:45:04 +0700

    first edit

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,51 @@
 #include <Arduino.h>
 
-const int LED_RED = 16;
+const int LED_RED = 17;
+const int LED_YELLOW = 16;
+const int LED_GREEN = 4;
 const int buttonPin = 2;
+
+void NormalMode();
 
 void setup() {
   pinMode(LED_RED, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
   Serial.begin(115200);
-  Serial.println("Hello, ESP32!");
+}
+
+void ButtonPressed() {
+  if (digitalRead(buttonPin) == LOW) {
+    Serial.println("Button pressed!");
+    digitalWrite(LED_YELLOW, HIGH);
+    delay(500);
+    digitalWrite(LED_YELLOW, LOW);
+    delay(500);
+  }
+}
+
+void NormalMode() {
+  digitalWrite(LED_RED, HIGH);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, LOW);
+  delay(5000);
+
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_YELLOW, HIGH);
+  digitalWrite(LED_GREEN, LOW);
+  delay(2000);
+
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, HIGH);
+  delay(5000);
 }
 
 void loop() {
-  int buttonState = digitalRead(buttonPin);
-  if (buttonState == LOW) {
-    Serial.println("Button pressed!"); 
-      digitalWrite(LED_RED, HIGH);
+if (digitalRead(buttonPin) == LOW) {
+    ButtonPressed();
   } else {
-    Serial.println("Button released!");
-    digitalWrite(LED_RED, LOW);
+    NormalMode();
   }
 }
